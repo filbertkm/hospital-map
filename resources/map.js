56b5f65912b43b620753488ba41d04114e54d51a
Willi <willi@jups42.de>
2012-12-01 14:58:59 +0100

    added layer control, converted spaces to tabs

--- resources/map.js
+++ resources/map.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
 
-	var map = L.map( 'map' );
+	var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+	});
 
-	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	   maxZoom: 18,
-	   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-	  }).addTo(map);
+	var hospitalLayer = L.tileLayer("http://overpass-api.de/api/interpreter?data=[out:json];node[amenity=hospital](52.34,13.3,52.52,13.6);out;");
 
 	function onLocationFound(e) {
-	  var radius = e.accuracy / 2
-
-	  L.marker(e.latlng).addTo(map)
-	  	.bindPopup("You are within " + radius + " meters from this point").openPopup();
+		var radius = e.accuracy / 2;
+		L.marker(e.latlng).addTo(map);
+		.bindPopup("You are within " + radius + " meters from this point").openPopup();
 	}
 
 	function onLocationError(e) {
-	  alert(e.message);
+		alert(e.message);
 	}
+
+	var map = L.map( 'map', {
+		layers: [tileLayer, hospitalLayer]
+	});
 
 	map.on('locationfound', onLocationFound);
 	map.on('locationerror', onLocationError);
 
 	map.locate({setView: true, maxZoom: 16});
-})( jQuery );
+	L.control.layers(null, {
+		"Hospitals" : hospitalLayer
+	}).addTo(map);
+});
