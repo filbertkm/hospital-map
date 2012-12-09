ee1ad2aed2ffa67906be06a978d7e8cbbe8d5316
Christopher Baines <cbaines8@gmail.com>
2012-12-09 09:45:33 +0000

    Removed redundant code

--- resources/op2geojson.js
+++ resources/op2geojson.js
@@ -76,13 +76,6 @@ op2geojson = function() {
 		return collection;
 	}
 
-	instance.geojson = function() {
-		url = "http://overpass-api.de/api/interpreter?data=[out:json];node[amenity=hospital](52.34,13.3,52.52,13.6);out;";
-		instance.fetch(url, function(data) {
-			return data;
-		});
-	}
-
 	return instance;
 
 };
